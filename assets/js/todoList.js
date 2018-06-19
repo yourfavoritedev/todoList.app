e36c8bfabd3a42bad1f281e01c02e801fe6b3fb0
yourfavoritedev <ChristopherNgo94@gmail.com>
2018-06-18 20:53:07 -0700

    Version 1.0 Commit
    Added feature. If the user completes all their tasks, they will be congratulated.

--- assets/js/todoList.js
+++ assets/js/todoList.js
@@ -1,12 +1,16 @@
+var items = $("ul li")
+
 //Will toggle status of item
 $("ul").on("click", "li", function(){
-	$(this).toggleClass("done")
+	$(this).toggleClass("done");
 })
 
 //Removes item when clicked
 $("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove()
+		items = $("ul li")
+		congratulate()
 	})
 	//stops events from outer events from firing
 	event.stopPropagation();
@@ -23,6 +27,8 @@ $("input[type='text']").keypress(function(event){
 		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + newItem + "</li>")
 		//Clears input
 		$(this).val("")
+		items = $("ul li")
+		removeCongrats()
 	}
 })
 
@@ -30,3 +36,15 @@ $("input[type='text']").keypress(function(event){
 $(".fa-plus").click(function(){
 	$("input[type='text']").fadeToggle()
 })
+
+function congratulate(){
+	if(items.length == 0){
+		$("ul").append("<li class='applaud'><span><i class='fa fa-trash'></i></span> " + "You have been really productive!" + "</li>")
+	}
+}
+
+function removeCongrats(){
+	if(items.length > 1){
+		$(".applaud").remove()
+	}
+}
